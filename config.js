6c67b7144ab5e2e6553dcc8caafd60c089ba404b
Htam85 <86758531+Htam85@users.noreply.github.com>
2021-07-01 11:05:20 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Bae ơiii!',
+    introDesc: `Nói cho nghe cái này nè`,
+    btnIntro: 'continue',
+    title: 'Thương tui hong😙????',
+    desc: 'Phải chăng bae thương tui nhiều lắm nhờ??? ',
+    btnYes: 'Thương chứ sao không <33',
+    btnNo: 'm mơ à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao thương t dữ dọ ??? <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Thương thì thương mà không thương cũng là thương <33333333',
+    mess: 'tui biết mà, ahihi🥰. Mãi thương nhiều nhiều 😘😘',
+    messDesc: 'nào hết dịch làm cái kèo to to nhá.',
+    btnAccept: 'Okiiiii lunnn <3',
+    messLink: 'hhttps://www.youtube.com/watch?v=XRApso9_H44'
+}
